6f995a073c4b613c800f583b3406f58317ad0d01
Ikem-David <ikemdavid8@icloud.com>
2026-09-14 20:35:03 +0100

    Integrated API to fetch Products for the FrontEnd

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from Routers import product
 from Routers import purchases
 from Routers import users
@@ -7,6 +8,13 @@ from Database import tables
 from Utils import cloudinary
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(users.router)
 app.include_router(product.router)
 app.include_router(purchases.router)
